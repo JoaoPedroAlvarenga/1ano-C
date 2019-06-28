ae65464acdf809ef4aefc866ae0a388be0ccfc39
João Pedro <41977137+JoaoPedroAlvarenga@users.noreply.github.com>
2019-06-28 17:57:22 -0300

    Pascal

---
+++ Pascal/Novo1.pas
@@ -0,0 +1,34 @@
+Program Pzim ;
+uses crt;
+var letra : char;
+ Begin
+  	write ('digite uma letra');
+     read (letra);
+ 
+     	if ((letra = 'a') or (letra = 'e') or (letra = 'i') or (letra = 'o') or (letra = 'u')) then
+          	write('é uma vogal')
+      else
+          write('não é uma vogal')
+  
+  
+ End.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Pascal/Novo2.pas
@@ -0,0 +1,17 @@
+Program Pzim ;
+uses crt;
+var letra : char;
+var nome: String; 
+ Begin
+  	write ('digite seu nome');
+     read (nome);
+     
+     if ((nome[1] = 'a') or (nome[1] = 'e') or (nome[1] = 'i') or (nome[1] = 'o') or (nome[1] = 'u')) then
+		write('É volgal')
+     else
+		write('nao É volgal')
+      
+  
+  
+  
+ End.
