195e4fa06fcb37dbb03fc64bedf6d6b6147b2a16
João Pedro <41977137+JoaoPedroAlvarenga@users.noreply.github.com>
2019-07-24 23:00:55 -0300

    Js

---
+++ Js/06.js
@@ -0,0 +1,5 @@
+var nome = "Pedro"
+var linguagem = "JavaScript"
+var frase = `Testando crase ${nome},Que tipo de linguagem usa crase ${nome}, só poderia ser ${linguagem} mesmo, python só usa um F na frente, mds, para que complicar tanto ${linguagem}`
+
+frase
