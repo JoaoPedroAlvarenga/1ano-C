6e241d113193a1ac71e10416c2dc4e03b5eca409
João Pedro Alvarenga <joaopedroaats@pm.me>
2020-06-22 14:54:18 -0300

    fix: resolve old problems

--- PHP/2 Ano/19-09-02/connection/read.php
+++ PHP/2 Ano/19-09-02/connection/read.php
@@ -4,7 +4,7 @@
 
     $query = "SELECT * FROM users"; 
 
-    $slq = mysqli_query($connection, $query ) or die(' Erro na query:' . $query . ' ' . mysql_error() )
+    $slq = mysqli_query($connection, $query ) or die(' Erro na query:' . $query . ' ' . mysqli_error($query));
 
     while ($row = mysqli_fetch_assoc( $slq )) { 
         print $row["cmpNome"] . " -- " . $row["cmpCpf"];
--- PHP/3 Ano/20-03-03/maiorNumero.php
+++ PHP/3 Ano/20-03-03/maiorNumero.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>oi oi</title>
 </head>
+
 <body>
 
-	<form method="post" >
+	<form method="post">
 		<h1>Nota 1</h1>
 		<input type="number" name="nota1">
 
@@ -16,27 +18,28 @@
 		<input type="submit" name="">
 	</form>
 
-<?php
+	<?php
 
-	$n1 = $_POST["nota1"]; 
+	$n1 = $_POST["nota1"];
 	$n2 = $_POST["nota2"];
 	$maior = [];
 	$menor = [];
 
-	if($n1 != $n2){
-		if($n1 > $n2){
-			$maior = [$n1,"nota de numero 1"];
-			$menor = [$n2,"nota de numero 2"];
+	if ($n1 != $n2) {
+		if ($n1 > $n2) {
+			$maior = [$n1, "nota de numero 1"];
+			$menor = [$n2, "nota de numero 2"];
 		}
-		$menor = [$n1,"nota de numero 1"];
-		$maior = [$n2,"nota de numero 2"];
+		$menor = [$n1, "nota de numero 1"];
+		$maior = [$n2, "nota de numero 2"];
 
-		echo("O numero maior e a $maior[0] com o valor de $maior[1]");
-		echo("Esse numero e maior por" $maior[0])
-	}else{
-		echo("O numeros sao iguais");
+		echo ("O numero maior e a $maior[0] com o valor de $maior[1]");
+		echo ("Esse numero e maior por" . $maior[0]);
+	} else {
+		echo ("O numeros sao iguais");
 	}
-?>
+	?>
 
 </body>
+
 </html>
