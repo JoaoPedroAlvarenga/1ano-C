4c275321435c0a78ff8ee55b6aeaf0a409ff44dc
João Pedro Alvarenga <joaopedroaats@pm.me>
2020-06-23 08:03:13 -0300

    feat(php): create menu link

--- PHP/3 Ano/20-06-22/secret/app.php
+++ PHP/3 Ano/20-06-22/secret/app.php
@@ -15,11 +15,19 @@ include('../php/verify_login.php')
 </head>
 
 <body>
-  <div class="img-background"></div>
-  <section>
-    <h1>Bem vindo(a) <?php echo $_SESSION['username']; ?></h1>
-    <h1> <a href="../php/logout.php">Sair</a></h1>
-  </section>
+  <div class="app">
+    <div class="img-background"></div>
+    <section class="menu">
+      <h1>Bem vindo(a) <?php echo $_SESSION['username']; ?></h1>
+      <ul>
+        <li><a href="">Cadastro</a></li>
+        <li><a href="">Consulta</a></li>
+        <li><a href="">Busca</a></li>
+      </ul>
+      <h1> <a href="../php/logout.php">Sair</a></h1>
+    </section>
+
+  </div>
 </body>
 
 </html>
